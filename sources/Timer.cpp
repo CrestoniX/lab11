e783cf9ee2ec4225a2825c1ca7f80e40d5b55ce1
CrestoniX <loltm.crav4enko@gmail.com>
2020-05-29 07:26:39 -0700

    final

--- sources/Timer.cpp
+++ sources/Timer.cpp
@@ -8,7 +8,7 @@ void Timer::create(std::unique_ptr<ThreadData> &data)
         return;
     }
 
-    while (Settings::timeout > 0 && (!data || !data->isTerminated)) { //булева переменная, инициализированная в Utils.h
+    while (Settings::timeout > 0 && (!data || !data->isTerminated)) {
         static const int DATA_TIMEOUT = 50;
 
         std::this_thread::sleep_for(milliseconds{DATA_TIMEOUT});
@@ -18,9 +18,9 @@ void Timer::create(std::unique_ptr<ThreadData> &data)
     data->isTerminated = true;
 
     try {
-        data->currentProcess.terminate(); //currentProcess - boost::process::child ThreadData::currentProcess, terminate - завершение
+        data->currentProcess.terminate();
     } catch (std::exception &exception) {
-        BOOST_LOG_TRIVIAL(error) << exception.what(); // посимвольный вывод кода ошибки
+        BOOST_LOG_TRIVIAL(error) << exception.what();
     }
 
     BOOST_LOG_TRIVIAL(info) << "Timeout";
